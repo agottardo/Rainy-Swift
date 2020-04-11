4344351f320e31795f1e0314ba627ab3f023ad9d
Andrea Gottardo <andrea@gottardo.me>
2020-04-11 16:52:16 -0700

    Add four days of daily forecasts to the home screen.

---
+++ Rainy/GUI/Home Screen/Cells/CurrentInfoTableViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  CurrentInfoTableViewCell.swift
+//  Rainy
+//
+//  Created by Andrea Gottardo on 2020-04-06.
+//  Copyright © 2020 Andrea Gottardo. All rights reserved.
+//
+
+import UIKit
+
+class CurrentInfoTableViewCell: UITableViewCell, ReusableView, NibLoadableView {
+    @IBOutlet var stackView: UIStackView!
+
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .none
+        formatter.dateFormat = "EEE"
+        return formatter
+    }()
+
+    func configure(with data: WeatherUpdate?, location _: Location?) {
+        backgroundColor = Theme.current.accentTint
+        guard let data = data else {
+            Log.error("No data to fill current weather data.")
+            return
+        }
+
+        guard let dailyData = data.daily?.data else {
+            return
+        }
+        stackView.distribution = .fillEqually
+        stackView.arrangedSubviews.forEach {
+            stackView.removeArrangedSubview($0)
+            $0.removeFromSuperview()
+        }
+        var count = 0
+        for day in dailyData {
+            if count > 4 {
+                break
+            }
+            guard let timestamp = day.time else { continue }
+            let dayString = dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(timestamp)))
+            let titleLabel = UILabel()
+            titleLabel.textColor = .white
+            titleLabel.text = dayString.uppercased()
+            titleLabel.textAlignment = .center
+            titleLabel.font = .systemFont(ofSize: 13.0, weight: .semibold)
+            let iconView = UIImageView(image: day.icon?.icon)
+            iconView.tintColor = .white
+            let temperatureHighLabel = UILabel()
+            temperatureHighLabel.textColor = .white
+            temperatureHighLabel.text = day.temperatureHigh?.stringRepresentation()
+            temperatureHighLabel.textAlignment = .center
+            temperatureHighLabel.font = .systemFont(ofSize: 13.0, weight: .semibold)
+            let temperatureLowLabel = UILabel()
+            temperatureLowLabel.textColor = .white
+            temperatureLowLabel.text = day.temperatureLow?.stringRepresentation()
+            temperatureLowLabel.textAlignment = .center
+            temperatureLowLabel.font = .systemFont(ofSize: 12.0, weight: .regular)
+            let dayStackView = UIStackView(arrangedSubviews: [
+                titleLabel,
+                iconView,
+                temperatureHighLabel,
+                temperatureLowLabel,
+            ])
+            dayStackView.axis = .vertical
+            dayStackView.spacing = 2
+            dayStackView.alignment = .center
+            stackView.addArrangedSubview(dayStackView)
+            count += 1
+        }
+    }
+}
